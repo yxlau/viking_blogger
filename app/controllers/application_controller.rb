908c1420f683d50717e2850ed104c3b44a969955
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-02-08 14:23:00 +0800

    Sorcery authentication complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def require_login
+    unless logged_in?
+      redirect_to root_path
+      return false
+    end
+  end
 end
