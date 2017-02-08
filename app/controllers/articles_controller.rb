17cd86157eb991c5558721d47f00c092cd3bdc60
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-02-08 12:08:09 +0800

    Tagging feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    flash.notice = "Article '#{@article.title}' Updated!"
+    flash.notice = "Article '#{@article.title}' created!"
     redirect_to article_path(@article)
   end
 
@@ -28,5 +28,14 @@ class ArticlesController < ApplicationController
 
   def edit
     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
   end
 end
